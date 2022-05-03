e5c9e1755f653d2cbb81986bb8344cc7e096d3f5
hkgnp <ben@pngs.cc>
2022-05-03 15:55:25 +0800

    Feat: Able to psummarise block now

--- src/index.tsx
+++ src/index.tsx
@@ -1,83 +1,33 @@
 import "@logseq/libs";
-import { BlockEntity, IBatchBlock } from "@logseq/libs/dist/LSPlugin.user";
-import { recurseFirstCut } from "./recursiveHighlights";
 import { callSettings } from "./callSettings";
-import { createSecondCutBtn } from "./createSecondCutBtn";
-
-const uniqueIdentifier = () =>
-  Math.random()
-    .toString(36)
-    .replace(/[^a-z]+/g, "");
+import { BlockEntity } from "@logseq/libs/dist/LSPlugin";
+import { goSummarise } from "./utils";
 
 const main = async () => {
   console.log("logseq-psummarise-plugin loaded");
 
   callSettings();
 
+  // SUMMARISE BLOCK
+  logseq.Editor.registerBlockContextMenuItem("Psummarise block", async (e) => {
+    const arrBlk: BlockEntity[] = [];
+    console.log(e);
+    const blk = await logseq.Editor.getBlock(e.uuid);
+    arrBlk.push(blk);
+    goSummarise(arrBlk);
+  });
+
+  // SUMMARISE PAGE
   logseq.provideModel({
     async extract() {
-      ///////////////////
-      //// FIRST CUT ////
-      ///////////////////
-      // Get matches
       const pageBT: BlockEntity[] =
         await logseq.Editor.getCurrentPageBlocksTree();
-      const firstCutArr: { highlights: string[]; id: string }[] = [];
-
-      recurseFirstCut(pageBT, firstCutArr);
-
-      // Create heading block
-      const layerOneBlock: BlockEntity = await logseq.Editor.insertBlock(
-        pageBT[0].uuid,
-        `${
-          logseq.settings.layer1
-        } {{renderer :secondCut_${uniqueIdentifier()}}}`,
-        {
-          before:
-            pageBT[0].content.includes(":: ") &&
-            !pageBT[0].content.includes("id:: ")
-              ? false
-              : true,
-          sibling: true,
-        }
-      );
-
-      // Create batch block
-      const highlightsBatchBlks: Array<IBatchBlock> = [];
-      for (let h of firstCutArr) {
-        if (h.highlights === null) {
-          continue;
-        } else if (h.highlights.length === 1) {
-          const payload = {
-            content: `${h.highlights[0]} [${logseq.settings.highlightsRefChar}](${h.id})`,
-          };
-          highlightsBatchBlks.push(payload);
-        } else {
-          for (let i of h.highlights) {
-            const payload = {
-              content: `${i} [${logseq.settings.highlightsRefChar}](${h.id})`,
-            };
-            highlightsBatchBlks.push(payload);
-          }
-        }
-      }
-
-      await logseq.Editor.insertBatchBlock(
-        layerOneBlock.uuid,
-        highlightsBatchBlks,
-        {
-          before: false,
-          sibling: false,
-        }
-      );
-
-      // Create extract highlights button and implement hack to render it
-      createSecondCutBtn(layerOneBlock);
-      await logseq.Editor.editBlock(layerOneBlock.uuid, { pos: 1 });
-      await logseq.Editor.exitEditingMode();
-
-      // Open summary block in right sidebar
-      logseq.Editor.openInRightSidebar(layerOneBlock.uuid);
+      if (!pageBT)
+        logseq.App.showMsg(
+          "This function is not available in the rolling journal page. It can only be used on a single journal or regular page",
+          "error"
+        );
+      goSummarise(pageBT);
     },
   });
 
---
+++ src/utils.ts
@@ -0,0 +1,69 @@
+import { BlockEntity, IBatchBlock } from "@logseq/libs/dist/LSPlugin.user";
+import { recurseFirstCut } from "./recursiveHighlights";
+import { createSecondCutBtn } from "./createSecondCutBtn";
+
+const uniqueIdentifier = () =>
+  Math.random()
+    .toString(36)
+    .replace(/[^a-z]+/g, "");
+
+export const goSummarise = async (arrOfBlocks: BlockEntity[]) => {
+  ///////////////////
+  //// FIRST CUT ////
+  ///////////////////
+  // Get matches
+  const firstCutArr: { highlights: string[]; id: string }[] = [];
+
+  recurseFirstCut(arrOfBlocks, firstCutArr);
+
+  // Create heading block
+  const layerOneBlock: BlockEntity = await logseq.Editor.insertBlock(
+    arrOfBlocks[0].uuid,
+    `${logseq.settings.layer1} {{renderer :secondCut_${uniqueIdentifier()}}}`,
+    {
+      before:
+        arrOfBlocks[0].content.includes(":: ") &&
+        !arrOfBlocks[0].content.includes("id:: ")
+          ? false
+          : true,
+      sibling: true,
+    }
+  );
+
+  // Create batch block
+  const highlightsBatchBlks: Array<IBatchBlock> = [];
+  for (let h of firstCutArr) {
+    if (h.highlights === null) {
+      continue;
+    } else if (h.highlights.length === 1) {
+      const payload = {
+        content: `${h.highlights[0]} [${logseq.settings.highlightsRefChar}](${h.id})`,
+      };
+      highlightsBatchBlks.push(payload);
+    } else {
+      for (let i of h.highlights) {
+        const payload = {
+          content: `${i} [${logseq.settings.highlightsRefChar}](${h.id})`,
+        };
+        highlightsBatchBlks.push(payload);
+      }
+    }
+  }
+
+  await logseq.Editor.insertBatchBlock(
+    layerOneBlock.uuid,
+    highlightsBatchBlks,
+    {
+      before: false,
+      sibling: false,
+    }
+  );
+
+  // Create extract highlights button and implement hack to render it
+  createSecondCutBtn(layerOneBlock);
+  await logseq.Editor.editBlock(layerOneBlock.uuid, { pos: 1 });
+  await logseq.Editor.exitEditingMode();
+
+  // Open summary block in right sidebar
+  logseq.Editor.openInRightSidebar(layerOneBlock.uuid);
+};
